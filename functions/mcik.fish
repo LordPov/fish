13fdd3d25423b21df158d72c909341200014207a
Brendon Ryan <brendon.ryan@imcpacific.com.au>
2014-02-27 12:01:29 +1100

    Added mcik and mucik (adds Kindle profile)

---
+++ functions/mcik.fish
@@ -0,0 +1,3 @@
+function mci -d "Maven clean install, skip tests, Kindle profile enabled"
+    mvn -Pkindle clean install -DskipTests $argv
+end
---
+++ functions/mucik.fish
@@ -0,0 +1,3 @@
+function muci -d "Maven clean install with updates, skip tests, Kindle profile enabled"
+    mvn -U -Pkindle clean install -DskipTests $argv
+end
